8a0fe9789400a58c3975d524b5f7964ac4102249
daleinen7 <daleinen@gmail.com>
2021-10-14 14:12:31 -0500

    add solution to remove 'new Function'

--- recursionSolution.js
+++ recursionSolution.js
@@ -19,20 +19,30 @@ const solution = (input, equalsNumber) => {
 			expression = formatExpression(s, input);
 			if (new Function(`return ${expression}`)() === equalsNumber) {
 				let answer = `${expression} = ${equalsNumber}`;
-				console.log(answer);
+				// console.log(answer);
 				return answer;
 			}
 		} else {
-			for (let i = 0; i < base; i++) {
-				// returns first iteration rather than calling all iterations and returning value
-				generate(s + i, slots);
-			}
+			generate(s + 0, slots);
+			generate(s + 1, slots);
+			generate(s + 2, slots);
+
+			// if (s[s.length - 1] === operators[0]) return generate(s + 1, slots);
+			// if (s[s.length - 1] === operators[1]) return generate(s + 2, slots);
+			// if (s[s.length - 1] === operators[2]) return generate(s, slots);
+
+			// for (let i = 0; i < base; i++) {
+			// 	// returns first iteration rather than calling all iterations and returning value
+			// 	generate(s + i, slots);
+			// }
+			// return generate(s + 0, slots);
+			// if (s[s.length - 1] === operators[0]) return generate()
 		}
 	};
 
 	return generate('', slots);
 };
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const nums = [1, 2, 3];
 
 console.log(solution(nums, 100));
--- solution.js
+++ solution.js
@@ -1,17 +1,52 @@
+const { performance } = require('perf_hooks');
+
 const solution = (input, equalsNumber) => {
+	const startTime = performance.now();
+
 	const operators = ['', '-', '+'];
+
 	const base = operators.length;
-	const slots = input.length - 1;
+	const slots = input.length;
 
 	let iteration = 0;
+
+	const answer = [];
 
 	// takes current number (configuration of operators) and zippers it with input
 	const formatExpression = (opSet, numSet) =>
 		numSet
-			.flatMap((el, idx) =>
-				idx < numSet.length - 1 ? [el, operators[opSet[idx]]] : [el]
+			.map((el, idx) =>
+				operators[opSet[idx]] === ''
+					? el
+					: ' ' + operators[opSet[idx]] + ' ' + el
 			)
 			.join('');
+
+	// takes current number (configuration of operators) and returns what the expression evaluates to
+	const evaluateExpression = (expression) => {
+		const expressionArr = expression.split(' ');
+		let currentOperator = '+';
+		let runningValue = 0;
+		expressionArr.forEach((el) => {
+			if (isNaN(parseInt(el))) {
+				currentOperator = el;
+			} else {
+				el = parseInt(el);
+				switch (currentOperator) {
+					case '-':
+						runningValue -= el;
+						break;
+
+					case '+':
+						runningValue += el;
+						break;
+					default:
+						break;
+				}
+			}
+		});
+		return parseInt(runningValue);
+	};
 
 	// while number being checked is less than possible outcomes (converted to base)
 	while (iteration <= base ** slots) {
@@ -19,20 +54,50 @@ const solution = (input, equalsNumber) => {
 
 		// combine input and baseIteration
 		expression = formatExpression(baseIteration, input);
+
 		// if this equals our number return it
-		if (new Function(`return ${expression}`)() === equalsNumber) {
-			const answer = `${expression} = ${equalsNumber}`;
-			return answer;
+		if (evaluateExpression(expression) === equalsNumber) {
+			if (expression[0] === ' ') {
+				const correct = `${expression} = ${equalsNumber}`;
+				answer.push(correct);
+			}
 		}
 
 		// otherwise go to the next number
 		iteration++;
 	}
 
+	const endTime = performance.now();
+
+	const totalTime = endTime - startTime;
+
 	// in case there is no solution
-	return 'no solution';
+	if (answer.length > 0) {
+		return (
+			answer.map((ans) => {
+				return ans + '\n';
+			}) +
+			'solution count: ' +
+			answer.length +
+			'\n' +
+			'total time: ' +
+			totalTime
+		);
+	} else {
+		return 'no solution';
+	}
 };
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
-console.log(solution(nums, 100));
+readline.question('Target number: ', (targetNumber) => {
+	readline.question('Space deliniated number set:', (numberSet) => {
+		const nums = numberSet.split(' ');
+
+		console.log(solution(nums, parseInt(targetNumber)));
+		readline.close();
+	});
+});
